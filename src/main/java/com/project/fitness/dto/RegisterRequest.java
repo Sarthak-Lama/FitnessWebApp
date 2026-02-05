d2cac31e33ac09d952de1a9f05bf0d9f7b952d80
Sarthak Lama <sarthaklama068@gmail.com>
2026-02-05 15:13:09 +0545

    Input Validations

--- src/main/java/com/project/fitness/dto/RegisterRequest.java
+++ src/main/java/com/project/fitness/dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.project.fitness.dto;
 
 import com.project.fitness.model.UserRole;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email")
     private String email;
+
+ @NotBlank(message = "Password is required")
+ @Min(8)
+ @NotNull
+ @NotEmpty
     private String password;
+
     private String firstName;
     private String lastName;
     private UserRole role;
--- src/main/java/com/project/fitness/security/SecurityConfig.java
+++ src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.fitness.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,14 +8,18 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
+@RequiredArgsConstructor
 
 
 //Centralized security configurations
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
@@ -23,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated());
+        http.addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class);
     return http.build();
     }
 
